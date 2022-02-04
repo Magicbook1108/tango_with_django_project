289e46953a99c7b4f7412d3e36b4ceff18d141dd
Xiaoyang Bao <2575138b@student.gla.ac.uk>
2022-02-04 21:15:58 +0000

    'Chapter6'

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
-    context_dict = {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
+    pages = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = pages
 
     return render(request,'rango/index.html', context=context_dict)
 
@@ -10,3 +19,21 @@ def about(request):
     context_dict = {'boldmessage':'Xiaoyang Bao'}
 
     return render(request,'rango/about.html',context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request,'rango/category.html', context=context_dict)
